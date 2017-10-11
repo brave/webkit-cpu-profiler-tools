ac85b663c73023322aae91e4b704fe3e4fcea152
ａｙｕｍｉ  ｙｕ <ayumi@ayumiyu.com>
2017-10-11 20:49:01 +0000

    things

--- summary.js
+++ summary.js
@@ -6,7 +6,8 @@ const fs = require('fs')
 const HELP_MSG = 'node index.js [file]'
 
 const file = process.argv[process.argv.length - 1]
-const cpuProfile = new CPUProfile(file)
+const data = fs.readFileSync(file)
+const cpuProfile = new CPUProfile(data)
 const nodes = cpuProfile.getFilteredTimelineData()
 for (let node of nodes) {
   console.log(`${node.url}:${node.lineNumber},${node.functionName},${parseInt(node.total)}`)
